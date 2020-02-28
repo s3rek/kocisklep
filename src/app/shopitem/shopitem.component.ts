4c27da1ffe621441416b555ce1c9e2b587c4ad8d
Daniel Tulaj <s3rek92@gmail.com>
2020-02-28 23:50:29 +0100

    styling vol1

--- src/app/shopitem/shopitem.component.ts
+++ src/app/shopitem/shopitem.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { faPaw} from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-shopitem',
@@ -6,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shopitem.component.css']
 })
 export class ShopitemComponent implements OnInit {
+  fapaw=faPaw;
 
   constructor() { }
 
